fe27c54eae19f385e5936a75c018117dd19fb7d0
munir sanzar <msanzar87@gmail.com>
2021-10-02 03:39:53 -0400

    signin Completed

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class SignIn {
     public static void main(String[] args) {
-        String username = "Samantha";
-        String password = "Java <3";
+        String username = "Munir";
+        String password = "Java";
         
         /* Task 1 
             1. Pick up a username and password from the user.
@@ -11,10 +11,20 @@ public class SignIn {
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
         System.out.print("• Username: ");
+        String usernameEntry = scan.nextLine();
         //pick up username
         System.out.print("• Password: ");
+        String passwordEntry = scan.nextLine();
         //pick up password
 
+        while (!usernameEntry.equals(username) || !passwordEntry.equals(password)){
+            System.out.println("\nIncorrect, please try again!\n");
+            System.out.print("• Username: ");
+            usernameEntry = scan.nextLine();
+            System.out.print("• Password: ");
+            passwordEntry = scan.nextLine();
+        }
+        System.out.println("\nSign in successful. Welcome!");
         /* Task 2 
             1. Set up a loop that keeps running while the username OR password is incorrect. 
                When they get it wrong:
