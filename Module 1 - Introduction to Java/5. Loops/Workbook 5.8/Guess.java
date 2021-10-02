abc328c7acd4f81fc9c46495fd8731cbb9700d3e
munir sanzar <msanzar87@gmail.com>
2021-10-02 03:23:02 -0400

    guess Completed

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 
 public class Guess  {
     public static void main(String[] args) {
-
+        int myNumber = 2;
+        Scanner scan = new Scanner(System.in);
        /* Task 1 
             1. Store a "secret" number between 1 and 5. 
             2. Then, prompt the user to enter a guess.
        */
        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
+       int guess = scan.nextInt();
+
+       while (guess != myNumber){
+           System.out.print("Guess Again: ");
+           guess = scan.nextInt();
+       }
+        System.out.println("You got it!");
         
-       Scanner scan = new Scanner(System.in);
+
 
 
         /* Task 2 
