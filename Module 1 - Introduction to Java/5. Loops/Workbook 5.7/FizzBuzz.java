6b14afae7748458233375387a1bb358120ac15d5
munir sanzar <msanzar87@gmail.com>
2021-10-02 02:54:52 -0400

    FizzBuzz Completed

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -2,7 +2,16 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
        // Task 1 – Make a for loop that counts from 0 to 18. 
-       
+        for (int i = 0; i < 19; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println(i+" FizzBuzz");
+            }else if (i % 3 == 0){
+                System.out.println(i+" Fizz");
+            }else if(i % 5 == 0){
+                System.out.println(i+" Buzz");
+            }
+
+        }
        /* Task 2
              
             Beside each number:
