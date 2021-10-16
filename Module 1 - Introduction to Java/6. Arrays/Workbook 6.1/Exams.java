4d294301808c8fc686e90fbad6b48f95e71869ab
munir sanzar <msanzar87@gmail.com>
2021-10-16 18:44:49 -0400

    Conditional  completed

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
@@ -3,7 +3,7 @@ public class Exams {
         
         System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
         //Task 1: Create an array that stores 5 students: "Harry", "Neville", "Ron", "Hermione", "Seamus"
-
+        String[] students = new String[];
         //Task 2: Assign each student a seat according to their index in the array.
         //           print: " <student at index 0> ", you will take seat 0. 
 
---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-four/Pyramid61.java
@@ -0,0 +1,9 @@
+public class Pyramid61 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+          for (int j = 0; j < 10; j++) {
+              System.out.print(" * ");
+          }
+        }
+    }
+}
